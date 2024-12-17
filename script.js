91e3a1a5211c294f6c4303a1a0a099a4db428b48
arjun2002211 <arjunprajapat2002211@gmail.com>
2024-12-17 20:12:47 +0530

    project approximate completed.

---
+++ script.js
@@ -0,0 +1,10 @@
+let leftSidebar = document.querySelector('.left-sidebar');
+
+function closeMenu() {
+    leftSidebar.style.transform = "translateX(-100%)";
+}
+function openMenu() {
+    leftSidebar.style.transform = "translateX(0px)";
+    
+
+}
